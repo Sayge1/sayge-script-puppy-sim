91a95a5a331b4fa8e2b28b25567b0d0f938e1090
Sayge1 <101651725+Sayge1@users.noreply.github.com>
2023-02-25 16:05:46 +0300

    Update sayge.lua

--- sayge.lua
+++ sayge.lua
@@ -30,3 +30,26 @@ AutoTp:Cheat(
 	    end
 	end    
 )
+
+Autofarm:Cheat(
+	"Checkbox", -- Type
+	"Auto Bones", -- Name
+	function(State) -- Callback function
+		local Players = game.Players
+        local Player = Players.LocalPlayer
+
+        local Character = Player.Character or Player.CharacterAdded:Wait()
+        local HumRoot = Character:WaitForChild("HumanoidRootPart")
+
+        local Parts = game.Workspace.Tokens.Local
+        while State == true do
+            while task.wait(1) do
+                for _, Object in next, Parts:GetChildren() do
+                    if Object.Name == "Blue Yummy" or Object.Name == "Gold Yummy" or Object.Name == "Bomb" or Object.Name == "Hasty" or Object.Name == "Lucky Break" or Object.Name == "Dinamite" or Object.Name == "Gold Bomb" or Object.Name == "Blue Bomb" or Object.Name == "Magic Carrot" or Object.Name == "Triple Jump" or Object.Name == "Linking Beam" or Object.Name == "Bones Pumpkin" or Object.Name == "Rage" then
+                        Object.CFrame = HumRoot.CFrame
+                    end
+                end
+            end
+	    end
+	end    
+)
