99fcf489404c19323e8b1d34547fd36bfb9a4320
Sayge1 <101651725+Sayge1@users.noreply.github.com>
2023-02-26 22:34:06 +0300

    Update sayge.lua

--- sayge.lua
+++ sayge.lua
@@ -241,3 +241,13 @@ AutoTp:Cheat(
 	    end
 	end    
 )
+
+PlayerStats:Cheat(
+	"Checkbox", -- Type
+	"Anti Afk", -- Name
+	function(State9) -- Callback function
+		local bb=game:service'VirtualUser'
+		game:service'Players'.LocalPlayer.Idled:connect(function()
+		bb:CaptureController()bb:ClickButton2(Vector2.new()) 
+	end
+)
