cd32595ad4b4454c7107c42ff7b6ae42abd13afa
Sayge1 <101651725+Sayge1@users.noreply.github.com>
2023-04-20 22:46:18 +0300

    Update sayge.lua

--- sayge.lua
+++ sayge.lua
@@ -63,7 +63,7 @@ AutoTp:Cheat(
 
         local Parts = game.Workspace.Tokens.Local
         while turnfarmbones do
-            task.wait(sborvsego1)
+            task.wait(1)
             for _, Object in next, Parts:GetChildren() do
   
                 if Object.Name == "Bone" or Object.Name == "Coin" then
@@ -78,7 +78,7 @@ AutoTp:Cheat(
 
 AutoTp:Cheat(
 	"Checkbox", -- Type
-	"Auto Abilities", -- Name
+	"Auto Abilities (tween)", -- Name
 	function(State1) -- Callback function
 		turnautoabilities = State1
 		local TweenService = game:GetService("TweenService")
@@ -109,6 +109,32 @@ AutoTp:Cheat(
         	end
 		end
 	end
+)
+
+AutoTp:Cheat(
+	"Checkbox", -- Type
+	"Auto Abilities", -- Name
+	function(State_abilities) -- Callback function
+		turnabilities = State_abilities
+	    local Players = game.Players
+        local Player = Players.LocalPlayer
+
+        local Character = Player.Character or Player.CharacterAdded:Wait()
+        local HumRoot = Character:WaitForChild("HumanoidRootPart")
+
+        local Parts = game.Workspace.Tokens.Local
+        while turnabilities do
+            task.wait(1)
+            for _, Object in next, Parts:GetChildren() do
+  
+                if Object.Name == "Blue Yummy" or Object.Name == "Gold Yummy" or Object.Name == "Bomb" or Object.Name == "Hasty" or Object.Name == "Lucky Break" or Object.Name == "Dinamite" or Object.Name == "Gold Bomb" or Object.Name == "Blue Bomb" or Object.Name == "Magic Carrot" or Object.Name == "Triple Jump" or Object.Name == "Linking Beam" or Object.Name == "Bones Pumpkin" or Object.Name == "Rage" then
+                    Object.CFrame = HumRoot.CFrame
+                end
+            end
+		
+
+	    end
+	end    
 )
 
 AutoTp:Cheat(
